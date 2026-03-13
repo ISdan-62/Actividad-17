b220b45e607f7f428bd94161b4356d411f4b6cf6
erick <erick@LEGI0SORIO>
2026-03-13 00:24:17 -0600

    E25

---
+++ 25/Program.cs
@@ -0,0 +1,16 @@
+﻿void areaRectangulo ()
+{
+    int altura = 0;
+    int vase = 0;
+    Console.WriteLine("Ingrese la altura del rectangulo: ");
+    altura = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la base del rectangulo: ");
+    vase = int.Parse(Console.ReadLine());
+    int area(int altura, int vase)
+    {
+        return altura * vase;
+    }
+    int resultado = area(altura, vase);
+    Console.WriteLine($"El area del rectangulo es {resultado}");
+}
+areaRectangulo();
