e9f3341d6e856b6d79110e092f58c9d412dd3ebd
erick <erick@LEGI0SORIO>
2026-03-12 19:58:15 -0600

    E7

---
+++ 7/Program.cs
@@ -0,0 +1,12 @@
+﻿double precio = 0;
+double descuento = 0;
+Console.Write("Ingrese el precio del producto: ");
+precio = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el porcentaje de descuento: ");
+descuento = double.Parse(Console.ReadLine());
+double precioFinal(double precio, double descuento)
+{
+    return precio - (precio * descuento / 100);
+}
+double resultado = precioFinal(precio, descuento);
+Console.WriteLine($"El precio final es: {resultado}");
