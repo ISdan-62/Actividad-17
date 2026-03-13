a592689f36b41c282eb4ca09f16609bf961b979a
erick <erick@LEGI0SORIO>
2026-03-12 21:33:25 -0600

    E11

---
+++ 11/Program.cs
@@ -0,0 +1,11 @@
+﻿double kilogramos = 0;
+Console.Write("Ingrese el peso en kilogramos: ");
+kilogramos = double.Parse(Console.ReadLine());
+double ConvertirALibras(double kilogramos)
+{
+    return kilogramos * 2.20462;
+}
+
+double resultado = ConvertirALibras(kilogramos);
+
+Console.WriteLine($"El peso en libras es: {resultado}");
