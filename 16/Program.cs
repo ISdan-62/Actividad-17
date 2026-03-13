93df7241b60af62bde212b5baa56e2e5da6356ca
erick <erick@LEGI0SORIO>
2026-03-12 23:14:16 -0600

    E16

---
+++ 16/Program.cs
@@ -0,0 +1,9 @@
+﻿double minutos = 0;
+Console.Write("Ingrese la cantidad de minutos: ");
+minutos = double.Parse(Console.ReadLine());
+double horas(double minutos)
+{
+    return minutos / 60;
+}
+double resultado = horas(minutos);
+Console.WriteLine($"El equivalente en horas es: {resultado}");
