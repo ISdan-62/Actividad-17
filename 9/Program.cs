fefffc17cb823c2d1adf36f2843d0ad7785222eb
erick <erick@LEGI0SORIO>
2026-03-12 21:23:05 -0600

    E9

---
+++ 9/Program.cs
@@ -0,0 +1,12 @@
+﻿double salarioHora = 0;
+double horasTrabajadas = 0;
+Console.Write("Ingrese el salario por hora: ");
+salarioHora = double.Parse(Console.ReadLine());
+Console.Write("Ingrese las horas trabajadas: ");
+horasTrabajadas = double.Parse(Console.ReadLine());
+double SalarioTotal(double salarioHora, double horasTrabajadas)
+{
+    return salarioHora * horasTrabajadas;
+}
+double resultado = SalarioTotal(salarioHora, horasTrabajadas);
+Console.WriteLine($"El salario total es: {resultado}");
