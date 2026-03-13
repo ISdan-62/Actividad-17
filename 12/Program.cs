3cfb3baf89a5f83ee8c362f31bdafc97e31cbd14
erick <erick@LEGI0SORIO>
2026-03-12 21:43:04 -0600

    E12

---
+++ 12/Program.cs
@@ -0,0 +1,9 @@
+﻿int edad = 0;
+Console.Write("Ingrese su edad: ");
+edad = int.Parse(Console.ReadLine());
+string MostrarEdad(int edad)
+{
+    return $"La edad ingresada es: {edad}";
+}
+string resultado = MostrarEdad(edad);
+Console.WriteLine($"{resultado}");
