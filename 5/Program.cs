c75c3dee8d51764e7880a0e62da97eadf1fb1d19
erick <erick@LEGI0SORIO>
2026-03-12 19:35:29 -0600

    E5

---
+++ 5/Program.cs
@@ -0,0 +1,13 @@
+﻿//Crear una función que reciba la base y la altura de un rectángulo y devuelva su área.
+int altura = 0;
+int vase = 0;
+Console.WriteLine("Ingrese la altura del rectangulo: ");
+altura=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la base del rectangulo: ");
+vase = int.Parse(Console.ReadLine());
+int area(int altura, int vase)
+{
+    return altura * vase;
+}
+int resultado = area(altura, vase);
+Console.WriteLine($"El area del rectangulo es {resultado}");
