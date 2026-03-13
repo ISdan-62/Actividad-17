f17f504a6a06feb721ca50f953301bb661dbf333
erick <erick@LEGI0SORIO>
2026-03-12 18:40:57 -0600

    E2

---
+++ 2/Program.cs
@@ -0,0 +1,10 @@
+﻿int numero = 0;
+Console.Write("Ingrese un número: ");
+numero = int.Parse(Console.ReadLine());
+
+int Cuadrado(int numero)
+{
+    return numero * numero;
+}
+int resultado = Cuadrado(numero);
+Console.WriteLine($"El cuadrado del número es: {resultado}");
