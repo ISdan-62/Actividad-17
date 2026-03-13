791d1c1ff12c0eae6ec333415b5b4fd5582c655b
erick <erick@LEGI0SORIO>
2026-03-13 00:40:37 -0600

    E27

---
+++ 27/Program.cs
@@ -0,0 +1,9 @@
+﻿void precioDescuento()
+{
+    Console.Write("Ingrese el precio del producto: ");
+    double precio = double.Parse(Console.ReadLine());
+    double descuento = precio * 0.10;
+    double precioFinal = precio - descuento;
+    Console.WriteLine($"El precio con descuento es: {precioFinal}");
+}
+precioDescuento();
