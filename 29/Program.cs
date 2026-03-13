960d661b555523227b07b84862fbb40a53db77de
erick <erick@LEGI0SORIO>
2026-03-13 00:52:53 -0600

    E29

---
+++ 29/Program.cs
@@ -0,0 +1,7 @@
+﻿void CantidadEstudiantes()
+{
+    Console.Write("Ingrese la cantidad de estudiantes del curso: ");
+    int estudiantes = int.Parse(Console.ReadLine());
+    Console.WriteLine($"En la clase hay {estudiantes} estudiantes.");
+}
+CantidadEstudiantes();
