869ddc313e25fff152f142451c3e37246ca0b537
erick <erick@LEGI0SORIO>
2026-03-12 22:12:46 -0600

    E13

---
+++ 13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("¿Cuántos productos desea ingresar?: ");
+int cantidad = int.Parse(Console.ReadLine());
+double TotalCompra(int cantidad)
+{
+    double total = 0;
+    for (int i = 1; i <= cantidad; i++)
+    {
+        Console.Write($"Ingrese el precio del producto {i}: ");
+        double precio = double.Parse(Console.ReadLine());
+        total += precio;
+    }
+    return total;
+}
+double resultado = TotalCompra(cantidad);
+Console.WriteLine($"El total de la compra es: {resultado}");
