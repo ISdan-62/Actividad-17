6ee68a74cce687befcd0a7302c41a6b7b51378ed
erick <erick@LEGI0SORIO>
2026-03-12 21:28:00 -0600

    E10

---
+++ 10/Program.cs
@@ -0,0 +1,9 @@
+﻿int numero = 0;
+Console.WriteLine("Ingrese un numero: ");
+numero=int.Parse(Console.ReadLine());
+int cuboNumero (int numero)
+{
+    return numero * numero * numero;
+}
+int resultado=cuboNumero(numero);
+Console.WriteLine($"El cubo del numero ingresado es: {resultado}");
