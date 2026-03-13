9c12d116591ff9a4ae9aa1ed8a8aa346f47dc850
erick <erick@LEGI0SORIO>
2026-03-12 22:25:05 -0600

    E15

---
+++ 15/Program.cs
@@ -0,0 +1,12 @@
+﻿int vase = 0;
+int altura = 0;
+Console.WriteLine("Ingrese la base del triangulo: ");
+vase=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la altura del triangulo: ");
+altura=int.Parse(Console.ReadLine());
+double area(double vase, double altura)
+{
+    return (vase*altura)/2;
+}
+double resultado=area(vase, altura);
+Console.WriteLine($"El area del triangulo es: {resultado}");
