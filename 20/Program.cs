cdcf4cf19e832f905b2de11a13d821b64ccff6e3
erick <erick@LEGI0SORIO>
2026-03-12 23:56:17 -0600

    E20

---
+++ 20/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ingrese la cantidad de días trabajados: ");
+int dias = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el pago diario: ");
+double pagoDiario = double.Parse(Console.ReadLine());
+double salarioTotal(int dias, double pagoDiario)
+{
+    return dias * pagoDiario;
+}
+double resultado = salarioTotal(dias, pagoDiario);
+Console.WriteLine($"El salario total del empleado es: {resultado}");
