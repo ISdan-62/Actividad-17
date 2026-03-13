ca1ad09fb7bae1d146f22732651fc841c83caa15
erick <erick@LEGI0SORIO>
2026-03-13 00:31:43 -0600

    E26

---
+++ 26/Program.cs
@@ -0,0 +1,9 @@
+﻿void Nacimiento()
+{
+    Console.Write("Ingrese su edad: ");
+    int edad = int.Parse(Console.ReadLine());
+    int añoActual = 2026;
+    int añoNacimiento = añoActual - edad;
+    Console.WriteLine($"Su año aproximado de nacimiento es: {añoNacimiento}");
+}
+Nacimiento();
