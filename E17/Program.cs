53119d563cb280cad9c1135f42eb99955a09a733
erick <erick@LEGI0SORIO>
2026-03-12 23:21:15 -0600

    E17

---
+++ E17/Program.cs
@@ -0,0 +1,15 @@
+﻿double suma = 0;
+Console.Write("Ingrese la cantidad de estudiantes: ");
+int cantidad = int.Parse(Console.ReadLine());
+double promedio(int cantidad)
+{
+    for (int repetir = 1; repetir <= cantidad; repetir++)
+    {
+        Console.Write($"Ingrese la nota del estudiante {repetir}: ");
+        double nota = double.Parse(Console.ReadLine());
+        suma += nota;
+    }
+    return suma / cantidad;
+}
+double resultado = promedio(cantidad);
+Console.WriteLine($"El promedio del grupo es: {resultado}");
