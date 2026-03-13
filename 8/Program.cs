513843daf31631e0aea17aa75da0722078ef086b
erick <erick@LEGI0SORIO>
2026-03-12 21:20:02 -0600

    P8

---
+++ 8/Program.cs
@@ -0,0 +1,15 @@
+﻿int numeroUno = 0;
+int numeroDos = 0;
+int numeroTres = 0;
+Console.WriteLine("Ingrese el primer numero: ");
+numeroUno=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero: ");
+numeroDos=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el tercer numero: ");
+numeroTres=int.Parse(Console.ReadLine());
+double promedio(int numeroUno, int numeroDos, int numeroTres)
+{
+    return (numeroUno + numeroDos + numeroTres) / 3;
+}
+double resultado=promedio(numeroUno, numeroDos, numeroTres);
+Console.WriteLine($"El promedio de los numero ingresados es: {resultado}");
