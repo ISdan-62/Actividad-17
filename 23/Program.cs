5e6637d545519bc9613b510af23e5b58a695e163
erick <erick@LEGI0SORIO>
2026-03-13 00:19:47 -0600

    E23

---
+++ 23/Program.cs
@@ -0,0 +1,11 @@
+﻿void SUMA()
+{
+    Console.WriteLine("Ingrese el primer numero: ");
+    int numeroUno=int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el segundo numero: ");
+    int numeroDos = int.Parse(Console.ReadLine());
+    int suma = 0;
+    suma = numeroUno + numeroDos;
+    Console.WriteLine($"La suma es de: {suma}");
+}
+SUMA();
