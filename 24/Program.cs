a1265859045f2b5a8daeb5f04701e896f02336d1
erick <erick@LEGI0SORIO>
2026-03-13 00:21:58 -0600

    E24

---
+++ 24/Program.cs
@@ -0,0 +1,13 @@
+﻿void areacirculo()
+{
+    double radio = 0;
+    Console.WriteLine("Ingrese el radio del circulo: ");
+    radio = double.Parse(Console.ReadLine());
+    double area(double radio)
+    {
+        return Math.PI * radio * radio;
+    }
+    double resultado = area(radio);
+    Console.WriteLine($"El area del circulo es: {resultado}");
+}
+areacirculo();
