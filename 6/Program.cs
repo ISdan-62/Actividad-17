d675d1467b10240f82b7d847f7c7c4d808e1b30c
erick <erick@LEGI0SORIO>
2026-03-12 19:49:44 -0600

    E6

---
+++ 6/Program.cs
@@ -0,0 +1,10 @@
+﻿double celsius = 0;
+Console.Write("Ingrese la temperatura en grados Celsius: ");
+celsius = double.Parse(Console.ReadLine());
+double fahrenheit(double celsius)
+{
+    return (celsius * 9 / 5) + 32;
+}
+double resultado = fahrenheit(celsius);
+
+Console.WriteLine($"La temperatura en Fahrenheit es: {resultado}");
