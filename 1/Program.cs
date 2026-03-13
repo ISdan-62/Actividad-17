bdf72b27466dcd3d6093be40b8048c8cf2ae5872
erick <erick@LEGI0SORIO>
2026-03-12 18:35:36 -0600

    E1

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿int numeroUno = 0;
+int numeroDos = 0;
+Console.Write("Ingrese un número: ");
+numeroUno = int.Parse(Console.ReadLine());
+Console.Write("Ingrese un número: ");
+numeroDos = int.Parse(Console.ReadLine());
+int Sumar(int numeroUno, int NumeroDos)
+{
+    return numeroUno + numeroDos;
+}
+int resultado = Sumar(numeroUno, numeroDos);
+Console.WriteLine("La suma es: " + resultado);
+return 0;
