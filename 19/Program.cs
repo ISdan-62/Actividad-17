0369a04f66d41d04b816b988fd3527e4a87e2eba
erick <erick@LEGI0SORIO>
2026-03-12 23:45:12 -0600

    E19

---
+++ 19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ingrese el nombre del estudiante: ");
+string nombre= Console.ReadLine();
+Console.Write("Ingrese la primera calificacion del estudiante: ");
+double calificacion1=double.Parse(Console.ReadLine());
+Console.Write("Ingrese la segunda calificacion del estudiante: ");
+double calificacion2=double.Parse(Console.ReadLine());
+Console.Write("Ingrese la tercera calificacion del estudiante: ");
+double calificacion3=double.Parse(Console.ReadLine());
+double promedio(double calificacion1, double calificacion2, double calificacion3)
+{
+    return (calificacion1 + calificacion2 + calificacion3) / 3;
+}
+double resultado=promedio(calificacion1, calificacion2, calificacion3);
+Console.WriteLine($"El promedio de las calificaciones del estudiante {nombre} es de: {resultado}");
