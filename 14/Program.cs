62884248f9435b43cdc5a965f68bd47c7a27266a
erick <erick@LEGI0SORIO>
2026-03-12 22:19:35 -0600

    E14

---
+++ 14/Program.cs
@@ -0,0 +1,12 @@
+﻿double distancia = 0;
+double tiempoEmpleado = 0;
+Console.WriteLine("Ingrese la distancia recorrida: ");
+distancia=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el tiempo empleado: ");
+tiempoEmpleado=int.Parse(Console.ReadLine());
+double velocidad (double distancia, double tiempoEmpleado)
+{
+    return distancia/tiempoEmpleado;
+}
+double resultado=velocidad(distancia, tiempoEmpleado);
+Console.WriteLine($"La velocidad es: {velocidad}");
